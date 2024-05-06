92dae6b3972d56200b0c1a0f3750e41a19e08147
Marco Sampietro <sampietro.marco@iisgalvanimi.edu.it>
2024-05-06 08:59:59 +0000

    aggiornamento

--- login.py
+++ login.py
@@ -1,29 +1,13 @@
-from flask import Flask, render_template, request, jsonify
-import csv
+from flask import Flask, render_template
 app = Flask(__name__)
-
-def read_users_csv():
-    with open('data/dati_utenti.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        users = list(reader)
-    return users
 
 @app.route('/')
 def home():
     return render_template('login.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login')
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    users = read_users_csv()
-
-    for user in users:
-        if user['username'] == username and user['password'] == password:
-            return jsonify({'success': True, 'name': user['name'], 'surname': user['surname']})
     
-    return jsonify({'success': False, 'message': 'Username o password errati'})
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
